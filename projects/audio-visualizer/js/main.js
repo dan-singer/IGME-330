039a4b59a9404bf1d47850defcbae86cad73fffc
Dan Singer (RIT Student) <djs5435@ad.rit.edu>
2019-02-13 15:11:25 -0500

    Start square drawing

--- projects/audio-visualizer/js/main.js
+++ projects/audio-visualizer/js/main.js
@@ -1,19 +1,97 @@
 (function(){
 
+    function distSqr(vec) {
+        return (Math.pow(vec.x, 2) + Math.pow(vec.y, 2));
+    }
+
+    function subVectors(A, B) {
+        return {x: A.x - B.x, y: A.y - B.y};
+    }
+
+    function lerp(A, B, t) {
+        let AtoB = subVectors(B, A);
+        return {x: A.x + t * AtoB.x, y: A.y + t * AtoB.y };
+    }
+
+    class Shape 
+    {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+            this.verts = [];
+        }
+        render(ctx) { }
+    }
+
+    class Square extends Shape 
+    {
+        constructor(x, y, length) 
+        {
+            super(x, y);
+            this.verts = [
+                {x:this.x - length/2, y:this.y - length/2}, {x:this.x + length/2, y:this.y - length/2},
+                {x:this.x - length/2, y:this.y + length/2}, {x:this.x + length/2, y:this.y + length/2}
+            ];
+            // calculate the distance of the entire shape
+            this.distanceSqr = Math.pow(length * 4, 2);
+        }
+        /**
+         * Render the square using webaudio data
+         * @param {CanvasRenderingContext2D} ctx 
+         * @param {Uint8Array} freqData 
+         */
+        render(ctx, freqData) {
+            ctx.fillStyle = "white";
+            ctx.beginPath();
+            ctx.moveTo(this.verts[0].x, this.verts[0].y);
+            // store a counter representing the current percentage completed
+            let curPercentage = 0;
+            // for each line in the path
+            for (let i = 0; i < this.verts.length - 1; ++i) {
+                // determine it's length
+                let line = subVectors(this.verts[i+1], this.verts[i]);
+                let lineLengthSqr = distSqr(line);
+                // determine the percentage of it relative to the entire shape
+                let linePerc = lineLengthSqr / this.distanceSqr;
+                let samples = linePerc * freqData.length;
+                for (let j = 0; j < samples; ++j) {
+                    // draw curPercentage to curPercentage + newPercentage of the curve along this line
+                    let index = (curPercentage * this.freqData.length) + j;
+                    let t = j / samples;
+                    let pointOnLine = lerp(this.verts[i], this.verts[i+1], t);
+                    ctx.moveTo(pointOnLine.x, pointOnLine.y);
+                }
+                curPercentage += linePerc;
+            }
+            ctx.closePath();
+            ctx.stroke();
+
+        }
+    }
+
+
     let audio = {
         ctx: null,
+        /** @type {AnalyserNode} */
         analyser: null,
-        source: null
+        source: null,
+        byteFreqData: null
     };
     let domElements = {
         audio: null,
         canvas: null
+    };
+    let audioOptions = {
+        shape: "square"
     };
     /** @type {CanvasRenderingContext2D} */
     let drawCtx;
     let prevTime = 0;
     let isPlaying = false;
     window.onload = init;
+
+
+
 
     function setupAudioContext() {
         domElements.audio = document.querySelector("audio");
@@ -22,6 +100,7 @@
         audio.source = audio.ctx.createMediaElementSource(domElements.audio);
         audio.source.connect(audio.analyser);
         audio.analyser.connect(audio.ctx.destination);
+        audio.byteFreqData = new Uint8Array(audio.analyser.frequencyBinCount);
     }
 
     function setupCanvas() {
@@ -53,7 +132,7 @@
     function playLoop(dt) {
         drawCtx.fillStyle = "black";
         drawCtx.fillRect(0,0,drawCtx.canvas.width, drawCtx.canvas.height);
-        audio.analyser.get
+        audio.analyser.getByteFrequencyData(audio.byteFreqData);
     }
 
     function pausedLoop(dt) {
