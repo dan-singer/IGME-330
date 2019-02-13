dda5f261e8dc742c27475ac1ea44b3935d721b48
Dan Singer <dsinger.sax@gmail.com>
2019-02-13 10:53:18 -0500

    Setup project states

--- projects/audio-visualizer/js/main.js
+++ projects/audio-visualizer/js/main.js
@@ -1,9 +1,18 @@
 (function(){
 
-    let audio = {};
-    let domElements = {};
+    let audio = {
+        ctx: null,
+        analyser: null,
+        source: null
+    };
+    let domElements = {
+        audio: null,
+        canvas: null
+    };
     /** @type {CanvasRenderingContext2D} */
     let drawCtx;
+    let prevTime = 0;
+    let isPlaying = false;
     window.onload = init;
 
     function setupAudioContext() {
@@ -23,12 +32,46 @@
         }
         window.dispatchEvent(new FocusEvent("resize"));
         drawCtx = domElements.canvas.getContext("2d");
+        domElements.canvas.onclick = togglePlay;
     }
 
     function init() {
         setupAudioContext();
         setupCanvas();
+        requestAnimationFrame(update);
+    }
+
+    function togglePlay(){
+        isPlaying = !isPlaying;
+        if (isPlaying) {
+            domElements.audio.play();
+        } else {
+            domElements.audio.pause();
+        }
+    }
+
+    function playLoop(dt) {
         drawCtx.fillStyle = "black";
-        drawCtx.fillRect(0, 0, domElements.canvas.width, domElements.canvas.height);
+        drawCtx.fillRect(0,0,drawCtx.canvas.width, drawCtx.canvas.height);
+    }
+
+    function pausedLoop(dt) {
+        drawCtx.fillStyle = "black";
+        drawCtx.fillRect(0,0,drawCtx.canvas.width, drawCtx.canvas.height);
+        drawCtx.font = "36px Montserrat";
+        drawCtx.fillStyle = "white";
+        const pauseText = "Click to start";
+        let wid = drawCtx.measureText(pauseText).width;
+        drawCtx.fillText(pauseText, drawCtx.canvas.width/2 - wid/2, drawCtx.canvas.height/2 - 18);
+    }
+
+    function update(timestamp) {
+        requestAnimationFrame(update);
+        let dt = (timestamp - prevTime) / 1000;
+        if (isPlaying) 
+            playLoop(dt);
+        else 
+            pausedLoop(dt);
+        prevTime = timestamp;
     }
 })();
