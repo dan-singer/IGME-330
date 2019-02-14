ebad46d93a562a4554cb0e54cad463e7e52232cf
Dan Singer <dsinger.sax@gmail.com>
2019-02-13 23:28:20 -0500

    Get the square movin

--- projects/audio-visualizer/js/main.js
+++ projects/audio-visualizer/js/main.js
@@ -3,9 +3,17 @@
     function distSqr(vec) {
         return (Math.pow(vec.x, 2) + Math.pow(vec.y, 2));
     }
+    function magnitude(vec) {
+        return Math.sqrt(distSqr(vec));
+    }
 
     function subVectors(A, B) {
         return {x: A.x - B.x, y: A.y - B.y};
+    }
+
+    function normalize(vec) {
+        let len = magnitude(vec);
+        return {x: vec.x / len, y: vec.y / len};
     }
 
     function lerp(A, B, t) {
@@ -30,7 +38,7 @@
             super(x, y);
             this.verts = [
                 {x:this.x - length/2, y:this.y - length/2}, {x:this.x + length/2, y:this.y - length/2},
-                {x:this.x - length/2, y:this.y + length/2}, {x:this.x + length/2, y:this.y + length/2}
+                {x:this.x + length/2, y:this.y + length/2}, {x:this.x - length/2, y:this.y + length/2}
             ];
             // calculate the distance of the entire shape
             this.distanceSqr = Math.pow(length * 4, 2);
@@ -41,25 +49,33 @@
          * @param {Uint8Array} freqData 
          */
         render(ctx, freqData) {
+            ctx.strokeStyle = "white";
             ctx.fillStyle = "white";
+            ctx.lineWidth = 10;
             ctx.beginPath();
             ctx.moveTo(this.verts[0].x, this.verts[0].y);
             // store a counter representing the current percentage completed
             let curPercentage = 0;
             // for each line in the path
-            for (let i = 0; i < this.verts.length - 1; ++i) {
+            for (let i = 0; i < this.verts.length; ++i) {
                 // determine it's length
-                let line = subVectors(this.verts[i+1], this.verts[i]);
+                let second = i + 1 == this.verts.length ? 0 : i + 1;
+                let line = subVectors(this.verts[second], this.verts[i]);
+                console.log(line);
+                console.log(normalize(line));
                 let lineLengthSqr = distSqr(line);
                 // determine the percentage of it relative to the entire shape
                 let linePerc = lineLengthSqr / this.distanceSqr;
                 let samples = linePerc * freqData.length;
                 for (let j = 0; j < samples; ++j) {
                     // draw curPercentage to curPercentage + newPercentage of the curve along this line
-                    let index = (curPercentage * this.freqData.length) + j;
+                    let index = (curPercentage * freqData.length) + j;
                     let t = j / samples;
-                    let pointOnLine = lerp(this.verts[i], this.verts[i+1], t);
-                    ctx.moveTo(pointOnLine.x, pointOnLine.y);
+                    let pointOnLine = lerp(this.verts[i], this.verts[second], t);
+                    let perp = {x: line.y, y:-line.x};
+                    let perpNorm = normalize(perp);
+                    let multiplier = (freqData[index] / 255) * 20 * ctx.lineWidth;
+                    ctx.lineTo(pointOnLine.x + perpNorm.x * multiplier, pointOnLine.y + perpNorm.y * multiplier);
                 }
                 curPercentage += linePerc;
             }
@@ -88,6 +104,7 @@
     let drawCtx;
     let prevTime = 0;
     let isPlaying = false;
+    let square = null;
     window.onload = init;
 
 
@@ -97,6 +114,7 @@
         domElements.audio = document.querySelector("audio");
         audio.ctx = new (window.AudioContext || window.webkitAudioContext)();
         audio.analyser = audio.ctx.createAnalyser();
+
         audio.source = audio.ctx.createMediaElementSource(domElements.audio);
         audio.source.connect(audio.analyser);
         audio.analyser.connect(audio.ctx.destination);
@@ -117,6 +135,7 @@
     function init() {
         setupAudioContext();
         setupCanvas();
+        square = new Square(drawCtx.canvas.width/2, drawCtx.canvas.height/2, 500);
         requestAnimationFrame(update);
     }
 
@@ -133,6 +152,7 @@
         drawCtx.fillStyle = "black";
         drawCtx.fillRect(0,0,drawCtx.canvas.width, drawCtx.canvas.height);
         audio.analyser.getByteFrequencyData(audio.byteFreqData);
+        square.render(drawCtx, audio.byteFreqData);
     }
 
     function pausedLoop(dt) {
